cddfce879a5fba1acb596736e559c6436c3c6978
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-06-22 20:24:27 +0200

    jwt created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const { response } = require('express');
 const {validationResult} = require('express-validator');
+var bcrypt = require('bcryptjs');
 const User = require("../models/User");
+const { generateJWT } = require('../helpers/jwt');
 
 const createUser = async(req, res = response) => {
     //req es la petición, aqui viene lo que pide el usuario
@@ -20,15 +22,28 @@ const createUser = async(req, res = response) => {
         }
 
          user = new User(req.body); //creamos una nueva instancia del modelo User que hemos importado
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync(10);
+        //encriptación de contraseña introducida por el usuario
+        user.password = bcrypt.hashSync(password, salt)
+
+
         //guardar en DB
         await user.save();
+
+        //generar JWT
+        const token = await generateJWT(user.id, user.name);
+        console.log(token);
 
         res.status(201).json({
             ok: true,
             uid: user.id,
-            name: user.name
+            name: user.name,
+            token
         })
     } catch (error) {
+        console.log(error);
         res.status(500).json({
             ok:false,
             msg:"Please talk to administrator"
@@ -37,27 +52,53 @@ const createUser = async(req, res = response) => {
 
 }
 
-const loginUser = (req, res) => {
+const loginUser = async(req, res) => {
     const {
         email,
         password
     } = req.body;
 
-    const errors = validationResult(req);
-    //validacion si errors no está vacio, quiere decir que hay un error
-    if( !errors.isEmpty()) {
-        return res.status(400).json({
+    try {
+
+        //comprobar si ese email está registrado en DB
+        const  user = await User.findOne( {email} );
+        if (!user) {
+            return res.status(400).json({
+                ok:false,
+                msg:"No user with this email"
+            })
+        }
+
+        //Confirmar que el password para el email coincide con el de la DB
+        //devuelve true si coinciden y false en caso contrario
+        const validPassword = bcrypt.compareSync(password, user.password); //compara el password intro por el usuario con el de la DB
+        if( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: "password not correct"
+            })
+        }
+
+        //generar JWT
+        const token = await generateJWT(user.id, user.name);
+
+
+        //Todo ok
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
             ok:false,
-            errors: errors.mapped()
+            msg:"Please talk to administrator"
         })
     }
 
-    res.status(201).json({
-        ok: true,
-        msg: "login",
-        email,
-        password,
-    })
 }
 
 const renewToken = (req, res) => {
