8792985655768f1cb1cb87391449b31221624956
Pedro RivDom <66123144+RivDomCode@users.noreply.github.com>
2022-07-08 18:41:51 +0200

    validation to create an event and save events on DB done

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Event = require("../models/Event")
 
 const getEvents = (req, res=response) => {
     return res.status(201).json({
@@ -7,10 +8,28 @@ const getEvents = (req, res=response) => {
     })
 }
 
-const createEvent = (req, res=response) => {
-    //Verificar que tenemos el evento
-    console.log(req.body)
+const createEvent = async (req, res=response) => {
 
+    const event = new Event(req.body);
+
+    try {
+        event.user = req.uid   //el uid viene en la req, asi id al user del modelo
+
+        const savedEvent = await event.save();
+
+        res.json({
+            ok:true,
+            event: savedEvent
+        })
+
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({
+            ok:fale,
+            msg: "talk to admin"
+        })
+    }
 
     return res.status(201).json({
         ok:true,
--- routes/events.js
+++ routes/events.js
@@ -2,9 +2,13 @@
 // host + /api/events
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { fieldsValidator } = require('../middlewares/fields-validator');
 const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
 const router = Router();
 const { validateJWT } = require('../middlewares/validate-jwt');
+const { isDate } = require("../helpers/isDate");
+
 
 //Todas tienen que pasar la validación del JWT
 router.use( validateJWT);
@@ -13,7 +17,15 @@ router.use( validateJWT);
 router.get('/', getEvents );
 
 //Crear un event
-router.post("/", createEvent )
+router.post(
+    "/",
+    [
+        check('title', 'title is required').not().isEmpty(),
+        check('start', 'start date is required').custom(isDate), //Este lo creamos personalizado
+        check('end', 'end date is required').custom(isDate), //Este lo creamos personalizado
+        fieldsValidator
+    ],
+    createEvent )
 
 // Actualizar un evento
 router.post("/:id",updateEvent)
